be5875e2fcf4b5d789d9690b3fedee4f03bbd8c9
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-12-28 19:25:18 +0100

    add more image sources (see Readme)

--- README.md
+++ README.md
@@ -15,3 +15,21 @@ The Script will output the Images files in the following structure:<br>
 /images/weapon for the weapons
 /images/vehicle for the vehicles
 ```
+
+If you choose option ```-o zip``` or ```-o both``` then there will be a folder called ```zips``` with the following output:<br>
+```
+/zips/ped.zip
+/zips/weapon.zip
+/zips/vehicle.zip
+```
+
+# Source
+You can choose between this sources:<br>
+| Source  | Vehicles           | Peds  | Weapons | type | maintained | link
+| ------- | ------------------ | ------------------ | ------------------ | ------------------ | ------------------ | ------------------ |
+| alt:V (Docs) | :white_check_mark: | :white_check_mark: | :white_check_mark: | png | :white_check_mark: | https://docs.altv.mp
+| alt:V (Wiki) | :white_check_mark: | :white_check_mark: | :white_check_mark: | png | :x: | https://wiki.altv.mp
+| Rage | :white_check_mark: | :x: | :x: | png | :white_check_mark: | https://wiki.rage.mp
+| Fivem | :x: | :white_check_mark: | :x: | webp | :white_check_mark: | https://docs.fivem.net
+
+The Default is alt:V (Docs).
--- dump.py
+++ dump.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 # Nickwasused 2021
 import argparse
+from urllib3 import PoolManager
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--option", help="fetch | zip | both", default="fetch")
+parser.add_argument("-r", "--remote", help="alt:V | alt:V2 | rage | fivem", default="alt:V")
 args = parser.parse_args()
+
+pool = PoolManager()
 
 # Use Dumps from here: https://github.com/DurtyFree/gta-v-data-dumps
 
 source = [
-    { "link": "https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/peds.json", "object_type": "ped", "file_type": "png" },
-    { "link": "https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/vehicles.json", "object_type": "vehicle", "file_type": "png" },
-    { "link": "https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/weapons.json", "object_type": "weapon", "file_type": "png" }
+    { "link": "https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/peds.json", "object_type": "ped" },
+    { "link": "https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/vehicles.json", "object_type": "vehicle" },
+    { "link": "https://raw.githubusercontent.com/DurtyFree/gta-v-data-dumps/master/weapons.json", "object_type": "weapon" }
 ]
 
 def retrieve_file_paths(dirName):
@@ -30,14 +34,19 @@ def retrieve_file_paths(dirName):
     # return all paths
     return filePaths
 
-def getremoteimages():
-    from json import loads
-    from urllib3 import PoolManager
-    from os import mkdir
+def fetch_handler():
+    if args.remote == "alt:V":
+        getremoteimages_modern_docs("https://docs.altv.mp/gta/images/{}/models/{}.{}", "alt:V", "png")
+    elif args.remote == "alt:V2":
+        getremoteimages_old_wiki("https://wiki.altv.mp/wiki/File:{}.png", "https://wiki.altv.mp{}", "alt:V", "png")
+    elif args.remote == "rage":
+        getremoteimages_old_wiki("https://wiki.rage.mp/index.php?title=File:{}.png", "https://wiki.rage.mp{}", "rage", "png")
+    elif args.remote == "fivem":
+        getremoteimages_modern_docs("https://docs.fivem.net/{}/{}.{}", "https://docs.fivem.net{}", "fivem", "webp")
+
+def check_directories():
     from os.path import exists
-
-    pool = PoolManager()
-
+    from os import mkdir
     # check directories
     if (exists("./images") == False):
         mkdir("./images")
@@ -47,15 +56,28 @@ def getremoteimages():
         if (exists(directory) == False):
             mkdir(directory)
 
+def getremoteimages_modern_docs(remote_url, special_flag, file_type):
+    from json import loads
+    from os.path import exists
+
+    check_directories()
+
     # download or skip
     for source_object in source:
+        if special_flag == "fivem" and source_object["object_type"] != "ped":
+            print("Downloading {} is not supported on Fivem!".format(source_object["object_type"]))
+            continue
+
         data_request = pool.request('GET', source_object["link"])
         data = loads(data_request.data.decode('utf-8'))
 
         for data_object in data:
             name = data_object["Name"].lower()
-            link = "https://docs.altv.mp/gta/images/{}/models/{}.{}".format(source_object["object_type"], name, source_object["file_type"])
-            filename = "./images/{}/{}.{}".format(source_object["object_type"], name, source_object["file_type"])
+            if special_flag == "fivem":
+                link = remote_url.format(source_object["object_type"] + "s", name, file_type)
+            else:
+                link = remote_url.format(source_object["object_type"], name, file_type)
+            filename = "./images/{}/{}.{}".format(source_object["object_type"], name, file_type)
 
             if (exists(filename)):
                 print("SKIPPING: {}:{}".format(source_object["object_type"], name))
@@ -68,6 +90,54 @@ def getremoteimages():
                         image_file.write(chunk)
 
                 image_request.release_conn()
+
+def getremoteimages_old_wiki(remote_url, base_remote_url, weapon_fetch_type, file_type):
+    from os.path import exists
+    from os import mkdir
+    from re import compile
+    from json import loads
+    from bs4 import BeautifulSoup
+
+    check_directories()
+
+    regexraw = r"\/images\/[0-9a-zA-Z_]{1,}\/[a-zA-Z0-9_]{2}\/.{1,}.png"
+    regex = compile(regexraw)
+
+    for source_object in source:
+        data_request = pool.request('GET', source_object["link"])
+        data = loads(data_request.data.decode('utf-8'))
+
+        for data_object in data:
+            name = data_object["Name"].lower()
+            filename = "./images/{}/{}.{}".format(source_object["object_type"], name, file_type)
+
+            if (exists(filename)):
+                print("SKIPPING: {}:{}".format(source_object["object_type"], name))
+                continue
+            else:
+                print("Downloading: {}:{}".format(source_object["object_type"], name))
+                if source_object["object_type"] == "weapon" and weapon_fetch_type == "alt:V":
+                    link = remote_url.format(data_object["Name"].replace("WEAPON_", "").replace("_", "-").capitalize().replace("mk2", "Mk2") + "-icon")
+                elif source_object["object_type"] == "weapon" and weapon_fetch_type == "rage" or source_object["object_type"] == "ped" and weapon_fetch_type == "rage":
+                    print("Downloading Weapons from Rage is not supported!")
+                    break
+                else:
+                    link = remote_url.format(data_object["Name"].capitalize())
+                response = pool.request('GET', link)
+                decoded_html = response.data.decode("utf-8", errors='ignore')
+                soup = BeautifulSoup(decoded_html, 'html.parser')
+                links = soup.find_all("a", href=True)
+
+                for link in links:
+                    result = regex.match(link["href"])
+                    if (result != None):
+                        filelink = base_remote_url.format(link["href"])
+                        image_request = pool.request('GET', filelink, preload_content=False)
+                        with open(filename, "wb") as image_file:
+                            for chunk in image_request.stream(1024):
+                                image_file.write(chunk)
+                        image_request.release_conn()
+                        break
 
 def zipimages():
     from os import mkdir, remove
@@ -98,9 +168,9 @@ def zipimages():
 
 if __name__ == "__main__":
     if args.option == "fetch":
-        getremoteimages()
+        fetch_handler()
     elif args.option == "zip":
         zipimages()
     else:
-        getremoteimages()
+        fetch_handler()
         zipimages()
